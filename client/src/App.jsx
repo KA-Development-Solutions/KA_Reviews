61f604df81adea8f5c4b029bbcbed43bebbaabbd
Alexander-Sturgeon <Alexander.h.sturgeon@outlook.com>
2026-09-16 10:33:00 -0400

    feat(router): Hooked up and App.jsx wired chore: renamed pages folder to views feat(Nav): navbar created as stub

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+//Services
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import React, {useState, useEffect} from 'react';
-
+import { Analytics } from "@vercel/analytics/react"
+//Views
+import HomePage from './views/home/HomePage';
+import MainAuthView from './views/authentication/MainAuthView';
+import AboutPage from './views/about/AboutPage';
+import NavBar from './shared/navigation/NavBar';
 function App(){
 
     return(
-        <h1>KA_Reviews</h1>
+        <BrowserRouter>
+        <Routes>
+            <NavBar/>
+            <Route path='/' element={HomePage}/>
+            <Route path='/login' element={MainAuthView}/>
+            <Route path='/about' element={AboutPage}/>
+
+        </Routes>
+        {/* Vercel analitics */}
+        <Analytics/>
+        </BrowserRouter>
     )
 }
 
